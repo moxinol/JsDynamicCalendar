e93fd64c823cb93c2bf95fde52138393fb226631
moxinol <moxikuma@gmail.com>
2023-08-15 15:03:39 +0100

    adding functionality to calendar

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-const currentDate = document.querySelector(".current-date");
-daysTag = document.querySelector(".days");
+const currentDate = document.querySelector(".current-date"),
+daysTag = document.querySelector(".days"),
 prevNextIcon = document.querySelectorAll(".icons span");
 
 //getting new date, current year and month
@@ -11,20 +11,35 @@ const months=["January","February","March","April","May","June","July",
 "August","September","Octuber","November","December"];
 
 const renderCalendar = () => {
-    let lastDateMonth= new Date(currYear, currMonth + 1, 0).getDate();//get last date of previous month
-   let liTag = "";
+    let firstDayOfMonth = new Date(currYear, currMonth, 1).getDay(),//get first day of month
+    lastDateOfMonth = new Date(currYear, currMonth + 1, 0).getDate(),//get last date of month
+    lastDayOfMonth = new Date(currYear, currMonth, lastDateOfMonth).getDay(),//get last day of month
+    lastDateOfLastMonth= new Date(currYear, currMonth, 0).getDate();//get last date of previous month
+    let liTag = "";
 
-    for (let i = 1; i <= lastDateMonth; i++) {
-        liTag +=`<li>${i}</li>`;
+    for (let i = firstDayOfMonth; i > 0; i--) {//creating li of previous month last days
+        liTag += `<li class="inactive">${lastDateOfLastMonth - i + 1}</li>`;
+    }
+    
+    for (let i = 1; i <= lastDateOfMonth; i++) {//creating li of all days of current month 
+        liTag += `<li>${i}</li>`;
+        
+    }
+
+    for (let i = lastDayOfMonth; i < 6; i++) {//creating li of next month first days
+        liTag += `<li class="inactive">${i - lastDayOfMonth + 1}</li>`;
         
     }
     currentDate.innerText =`${months[currMonth]} ${currYear}`;
-    daysTag.innerHTML =liTag;
+    daysTag.innerHTML = liTag;
 }
 renderCalendar();
 
 prevNextIcon.forEach(icon => {
     icon.addEventListener("click", () => { //adding click event to both prev and next icons
-        console.log(icon);
+        //decrement or icrement by 1 current month based on icon that was clicked
+        currMonth = icon.id === "prev" ? currMonth - 1 : currMonth + 1;
+        renderCalendar();
+
     });
 });
